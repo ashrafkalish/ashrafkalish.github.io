4d694164e2f138a37de795704a1a5b96a0e5b305
Ash Kasibante <ash.kasibante@chss.org.uk>
2025-03-29 13:49:44 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# CVTemplate
+
+Many professionals showcase their achievements through their online websites. Here are some exmaples:
+- https://stevenbartlett.com/
+- https://www.deborahmeaden.com/
+- https://davidcthomas.co.uk/ (Portfolio website)
+- https://danielmoulton.me/ (HPE intern website)
+- https://kylethmas.github.io/ (My website)
+
+This is the starting template for this workshop: https://kylethmas.github.io/CVTemplate/
+
+## Starting From Scratch
+
+### Set up
+- Download visual studio code - https://code.visualstudio.com/Download
+- Download github desktop - https://desktop.github.com/
+- Create a github.com account
+- Fork and Clone this repository
+
+### Make it your own
+- Open the CV Template Folder in Visual Studio Code
+- Replace the CV template with your details
+- Add in more experiences and skills
+- Change the Colours/Layout/Order
+- Add in your uni courses
+- Add in your linkedin url
+
+### Optional Extension: Access it on the internet
+- Use Github Desktop to add it to github
+- Follow instructions on https://pages.github.com/ to see it on your phone
+- Select options project site then start from scratch and skip to step 4
---
+++ script.js
@@ -0,0 +1,53 @@
+// Author Kyle Thomas
+
+// Smooth Scrolling
+document.querySelectorAll('nav a[href*="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Toggle Nav menu on mobiles
+const navToggle = document.querySelector('.nav-toggle');
+const navMenu = document.querySelector('nav ul');
+
+navToggle.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+});
+
+// Form ajax
+var form = document.getElementById("my-form");
+  
+  async function handleSubmit(event) {
+    event.preventDefault();
+    var status = document.getElementById("my-form-status");
+    var data = new FormData(event.target);
+    fetch(event.target.action, {
+      method: form.method,
+      body: data,
+      headers: {
+          'Accept': 'application/json'
+      }
+    }).then(response => {
+      if (response.ok) {
+        status.innerHTML = "Thanks for your submission!";
+        form.reset()
+      } else {
+        response.json().then(data => {
+          if (Object.hasOwn(data, 'errors')) {
+            status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+          } else {
+            status.innerHTML = "Oops! There was a problem submitting your form"
+          }
+        })
+      }
+    }).catch(error => {
+      status.innerHTML = "Oops! There was a problem submitting your form"
+    });
+  }
+  form.addEventListener("submit", handleSubmit)
+
